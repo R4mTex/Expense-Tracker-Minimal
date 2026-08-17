852dccade1ed79fe82ffa6fab8c15bf1a18ddc1c
Simon <poutots@gmail.com>
2026-08-17 11:40:43 +0200

    set up pages + add an icon

--- front/src/components/Amount.tsx
+++ front/src/components/Amount.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import type { AmountInterface } from "../interfaces/amountInterface";
 
 function Amount(props: AmountInterface) {
+    const [clickedButton, setClickedButton] = useState<number | null>(null);
     return (
         <>
             <div className="border rounded-lg p-4 text-center">
@@ -11,8 +13,15 @@ function Amount(props: AmountInterface) {
                 <img className="h-32 w-48 object-cover mx-auto" src={props.image} />
 
                 <div className="flex flex-col items-center">
-                    <button>Like</button>
-                    <button>Add to Total Amount</button>
+                    <button
+                        id="1"
+                        className={`${clickedButton === 1 ? "cursor-pointer rounded-lg p-2 border-2 bg-red-500" : "cursor-pointer rounded-lg p-2 border-2 border-red-500"}`}
+                        onClick={() => setClickedButton(1)}>
+                        Like
+                    </button>
+                    <button id="2" className="cursor-pointer rounded-lg p-2 border-2 border-green-500">
+                        Add to Total Amount
+                    </button>
                 </div>
             </div>
         </>
--- front/src/components/NavigationBar.tsx
+++ front/src/components/NavigationBar.tsx
@@ -1,22 +1,50 @@
 import { useState } from "react";
+import organisationIcon from "../assets/icons/charm--organisation.svg";
 
 /*
 <button class="cursor-pointer ...">Submit</button>
 <button class="cursor-progress ...">Saving...</button> "cursor-pointer active:cursor-progress"
 <button class="cursor-not-allowed ..." disabled>Confirm</button>
 */
-const menuList: string[] = ["Voyage", "Logement", "Nourriture", "Divers"];
+interface MenuItem {
+    name: string;
+    path: string;
+}
+
+const menuList: MenuItem[] = [
+    {
+        name: "Expenses",
+        path: "/home",
+    },
+    {
+        name: "Travel",
+        path: "/travel",
+    },
+    {
+        name: "Housing",
+        path: "/housing",
+    },
+    {
+        name: "Food",
+        path: "/food",
+    },
+    {
+        name: "Deverses",
+        path: "/deverses",
+    },
+];
 function NavBar() {
-    const [clickedMenu, setClickedMenu] = useState<number | null>(null);
+    const [clickedMenu, setClickedMenu] = useState<string | null>(null);
     return (
         <>
             <div className="flex flex-col md:flex-row border justify-end">
-                {menuList.map((menu: string, index: number) => (
+                <img className="" src={organisationIcon} alt="organisation tree" height="25" width="50" />
+                {menuList.map((menu) => (
                     <button
-                        key={index}
-                        className={`p-2 ${clickedMenu === index ? "cursor-progress" : "cursor-pointer"}`}
-                        onClick={() => setClickedMenu(index)}>
-                        {menu}
+                        key={menu.path}
+                        className={`p-2 ${clickedMenu === menu.path ? "cursor-progress" : "cursor-pointer"}`}
+                        onClick={() => setClickedMenu(menu.path)}>
+                        {menu.name}
                     </button>
                 ))}
             </div>
